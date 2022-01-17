42a859a49e07d5c3c68995464bf831370bd81c99
azoozalharte <zezo50115@protonmail.com>
2022-01-17 15:18:38 +0300

    new_code branch

--- main.js
+++ main.js
@@ -5,32 +5,39 @@ const container = document.querySelector('.container');
 
 let myLibrary = []
 
-function Book(title, author, pages, readed, id ) {
+function Book(title, author, pages, readed) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readed = readed;
-    this.id = id
 }
 
 
-function deleteBookToLibrary() {
+function deleteBookToLibrary(id) {
     const deleteButton = document.querySelectorAll('.delete')
-    const idNum = document.querySelectorAll('.id-num')
     deleteButton.forEach(button => {
         button.addEventListener( 'click', function(e){
-            console.log(e);
+            for (const key in myLibrary) {
+                if (myLibrary[key].id == id) {
+                }
+            }
         })
     });
 }
 
 function addBookToLibrary() {
     const addButton = document.querySelector('#add');
-    let counter = 0;
     addButton.addEventListener('click', () => {
-        let newBook = new Book(titleInput.value, authorInput.value, pageNumInput.value, false, counter);
+        let newBook = new Book(titleInput.value, authorInput.value, pageNumInput.value, false);
         myLibrary.push(newBook);
-        counter++
+addBookToDom()
+
+    })
+}
+
+function addBookToDom() {
+    console.log(myLibrary.length);
+    myLibrary.forEach( book => {
         //card div
         const card = document.createElement('div')
         card.classList.add('cards')
@@ -38,50 +45,58 @@ function addBookToLibrary() {
         //delete button
         const deleteButton = document.createElement('button')
         deleteButton.classList.add('delete')
-        deleteButton.setAttribute('onclick', 'deleteBookToLibrary()');
-        deleteButton.onclick = function () {deleteBookToLibrary();}
         card.appendChild(deleteButton);
-        
+
         // icon
         const icon = document.createElement('i')
         icon.classList.add('fa', 'fa-trash')
         deleteButton.appendChild(icon)
-        
-        //id num 
-        const idNumber = document.createElement('div')
-        // id-num
-        icon.classList.add('id-num')
-        idNumber.innerHTML = counter
-        card.appendChild(idNumber);
-        
+
         // h1 title
         const h1Title = document.createElement('h1')
         h1Title.classList.add('title')
-        h1Title.innerHTML = titleInput.value.length > 0 ? titleInput.value : 'No title'
+        h1Title.innerHTML = book.title.length > 0 ? book.title : 'No title'
         card.appendChild(h1Title)
 
         //h4 by word
         const h4By = document.createElement('h4')
-        h4By.innerHTML = authorInput.value.length > 0 ? 'By' : 'No author'
+        h4By.innerHTML = book.author.length > 0 ? 'By' : 'No author'
         card.appendChild(h4By)
 
         // h3 author name
         const h3Author = document.createElement('h3')
         h3Author.classList.add('author')
-        h3Author.innerHTML = authorInput.value
+        h3Author.innerHTML = book.author
         card.appendChild(h3Author)
 
         // Span pages Number
         const spanPage = document.createElement('span')
         spanPage.classList.add('page-num')
-        spanPage.innerHTML = `${pageNumInput.value} pages`
+        spanPage.innerHTML = `${book.pageNumInput} pages`
         card.appendChild(spanPage)
 
         container.appendChild(card)
-        
-    })
+    });
 }
 
 
-
 addBookToLibrary()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let counter = 1;
+
+
+// counter++
+
