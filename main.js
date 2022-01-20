2dc615984c23f60ee1cbfb96b01a094a68d55c00
azoozalharte <zezo50115@protonmail.com>
2022-01-20 10:02:53 +0300

    book class

---
+++ main.js
@@ -0,0 +1,19 @@
+// Book Class: Represents a book
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+}
+
+
+// UI Class: Handle UI tasks
+
+//Store Class : Handles storage 
+
+// Event display book
+
+//Event Add Book
+
+//Event Remove Book
